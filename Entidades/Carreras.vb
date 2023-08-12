2b112613f6ac691eed33556efc1712b42eaf41c5
Yordan Salazar <yordan10_pv@hotmail.com>
2023-08-12 12:41:16 -0600

    Aguege atributo

--- Entidades/Carreras.vb
+++ Entidades/Carreras.vb
@@ -1,6 +1,8 @@
 ﻿Public Class Carreras
     Public Property codigo As String
     Public Property nombre As String
+    Public Property Apellido As String
+
     Public Property grado As String
     Public Property estado As Short
 
