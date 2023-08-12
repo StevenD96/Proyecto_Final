23cebcbf4e63ae527a2755d6bcd3c474596208b2
Steven Mora Chaves <Steven Daniel Mora@DESKTOP-K5PIQBB>
2023-08-12 12:45:11 -0600

    S

--- Entidades/Carreras.vb
+++ Entidades/Carreras.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Carreras
     Public Property codigo As String
     Public Property nombre As String
-    Public Property Apellido As String
+
 
     Public Property grado As String
     Public Property estado As Short
