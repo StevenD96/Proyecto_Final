c038368d26f4ad1f6daa6d78de2f188aa4599194
Steven Mora Chaves <Steven Daniel Mora@DESKTOP-K5PIQBB>
2023-08-19 12:32:18 -0600

    .

---
+++ Negocios/Funcionarios.vb
@@ -0,0 +1,15 @@
+﻿Public Class Funcionarios
+
+
+    Public Sub insertarFuncionario(ByVal funcionario As Entidades.Funcionarios)
+
+        Try
+            Dim datos As New Datos.DatosFuncionarios
+            datos.InsertarFuncionario(funcionario)
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+
+End Class
